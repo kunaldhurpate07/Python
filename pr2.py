917eb52eb499df29fd8569791549928a0e59f351
kunaldhurpate07 <dhurpatekunal5@gmail.com>
2025-05-14 12:01:52 +0530

    Your commit message

---
+++ pr2.py
@@ -0,0 +1,19 @@
+str = "hello world"
+
+#replace world with Kunal
+str.replace("world","Kunal" )
+print(str)
+
+a = [1,2,3,4]
+b=4
+c={2,3,4}
+d = (1,2,3,4)
+
+print(id(a[3]))
+print(id(b))
+# print(id(c[2]))
+print(id(d[3]))
+
+str1 = "HELLO" + (str[5:])
+print(str1)
+print(id(str1))
